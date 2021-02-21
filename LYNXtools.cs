8c4a0b702bda45369e6ddbeb4e2f20ec52110030
Aeneas <eaglephepheonix@gmail.com>
2021-02-21 13:34:46 +0000

    Add files via upload

--- LYNXtools.cs
+++ LYNXtools.cs
@@ -9,10 +9,7 @@ using System.IO;
 using System.Reflection;
 using BepInEx.Logging;
 using Logger = BepInEx.Logging.Logger;
-
-
-namespace LYNXtools
-{
+using BBI.Unity.Game;
     public class LYNXtools
     {
         
@@ -20,7 +17,15 @@ namespace LYNXtools
         public static string keyWord = ""; // because who doesn't need a safe word in unconsensual modding ;p
         public static ManualLogSource LogSource;
 
-        public static void Init(string bundleName, string keyword, bool requireBundle)
+
+
+    /// <summary>
+    /// Initialize Registry with needed data
+    /// </summary>
+    /// <param name="bundleName">Name of bundle to load</param>
+    /// <param name="keyword">UNIQUE keyword of your mod (This is used by the asset bundle loader to load the bundle)</param>
+    /// <param name="requireBundle">Do you need to load asset bundles</param>
+    public static void Init(string bundleName, string keyword, bool requireBundle)
         {
 
             LogSource = Logger.CreateLogSource("LYNXtools-" + keyword);
@@ -38,5 +43,46 @@ namespace LYNXtools
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }
+
+    /// <summary>
+    /// Creates a valid stickerAsset specified by the modder for use
+    /// </summary>
+    /// <param name="nameID"></param>
+    /// <param name="descID"></param>
+    /// <param name="IconPath"></param>
+    /// <param name="objectiveType"></param>
+    /// <param name="objective"></param>
+    /// <param name="currencyType"></param>
+    /// <param name="amountForSticker"></param>
+    /// <param name="salvageType"></param>
+    /// <param name="mustCompleteSingleShip"></param>
+    /// <param name="collectAllInstances"></param>
+    /// <param name="currencyInOneShip"></param>
+    /// <param name="moduleRequiredList"></param>
+    /// <param name="size"></param>
+    /// <param name="materialOverride"></param>
+    /// <returns></returns>
+    public static StickerAsset StickerCreatorV1(string nameID, string descID, string IconPath, StickerAsset.StickerObjectiveType objectiveType,
+        ObjectiveEntry objective, CurrencyAsset currencyType, float amountForSticker, SalvageableChangedEvent.SalvageableState salvageType,
+        bool mustCompleteSingleShip, bool collectAllInstances, float currencyInOneShip, List<ModuleConstructionAsset> moduleRequiredList, 
+        StickerSizeAsset size, Material materialOverride)
+    {
+
+        //Properties are private :( going to need to do it via bundle or custom write a patch which let's us set
+
+        StickerAsset sticker = new StickerAsset
+        {
+            
+
+
+
+
+        };
+
+        
+
+
+
+        return sticker;
     }
-}
+    }
