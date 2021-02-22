f4664d49a7de7302433feef8bc82407bab573c4a
Aeneas <eaglephepheonix@gmail.com>
2021-02-22 21:44:15 +0000

    Add files via upload

---
+++ Core/SpriteAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+using BepInEx.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx.Logging;
+using BBI.Unity.Game;
+using Logger = BepInEx.Logging.Logger;
+using BBI;
+using BBI.Unity.Game;
+
+namespace LYNXAPI.Core
+{
+    class SpriteAPI
+    {
+        public static Sprite CreateSprite(string Iconpath)
+        {
+            Sprite mySprite = InitialiserAPI.bundle.LoadAsset<Sprite>(Iconpath);
+            return mySprite;
+        }
+        public static StickerSizeAsset CreateStickerSizeAsset()
+        {
+            StickerSizeAsset stickerSize = new StickerSizeAsset();
+
+            return stickerSize;
+
+        }
+
+    }
+}
